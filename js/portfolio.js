589ebb7ffcde7f268463132c8fce88ddb5579c34
Sergey Poliakov <ps.force@gmail.com>
2016-12-05 17:51:54 +0200

    Classic design section

---
+++ js/portfolio.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    //image-sliders
+    $("#lightSlider").lightSlider({
+        item: 1,
+        thumbItem: 0,
+        gallery: false,
+        pager: false,
+        loop: true
+    });
+
+    $("#lightSlider2").lightSlider({
+        item: 1,
+        thumbItem: 0,
+        gallery: false,
+        pager: false,
+        loop: true
+    });
+
+    $("#lightSlider3").lightSlider({
+        item: 1,
+        thumbItem: 0,
+        gallery: false,
+        pager: false,
+        loop: true
+    });
+
+    //меню
+    //$('.nav-scroll').click(function () {
+    //    $('.nav-scroll').removeClass('wearehere');
+    //    $(this).addClass('wearehere');
+    //    var a_href = $(this).attr('href');
+    //    $('html, body').animate({
+    //        scrollTop: $(a_href).offset().top
+    //    }, 2000);
+    //});
+    //
+    //var $toggle = $('#nav-toggle');
+    //var $menu = $('#nav-menu');
+    //
+    //$toggle.click(function() {
+    //    $(this).toggleClass('is-active');
+    //    $menu.toggleClass('is-active');
+    //});
+
+});
